0aaeaf1e5d35eaa2413ed5e55e56bee8febea951
Alwin Doss <alwindoss84@gmail.com>
2019-09-18 22:31:13 +0530

    Added APIs for SignUp and ConfirmSignUp
    The 2 APIs added are crude it would require a followup commit

--- cognito.go
+++ cognito.go
@@ -2,6 +2,7 @@ package sully
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
@@ -13,7 +14,17 @@ import (
 	cip "github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
 )
 
+func empty(strs ...string) bool {
+	for _, s := range strs {
+		if s == "" {
+			return true
+		}
+	}
+	return false
+}
+
 // Config is the configuration that needs to be provided to the factory functon NewCognitoClient
+// IdentityPoolID can be empty if the client that is created using this config is not used for Authenticate API call
 type Config struct {
 	UserPoolID     string
 	IdentityPoolID string
@@ -23,23 +34,22 @@ type Config struct {
 
 // Cognito is
 type Cognito interface {
+	// Authenticate authenticates a user and if the authentication is successful
+	// it returns the token else returns empty string but a non nil error
 	Authenticate(userName, password string) (string, error)
-}
 
-func isEmpty(strs ...string) bool {
-	for _, s := range strs {
-		if s == "" {
-			return true
-		}
-	}
-	return false
+	// SignUp is used to register a user
+	SignUp(userName, emailID, password string) (string, error)
+
+	// ConfirmSignUp confirms the signup given the confirmation code
+	ConfirmSignUp(userName, confirmationCode string) (string, error)
 }
 
 // NewCognitoClient is a factory to create the cognito client
 // if the nil config is not provided or any of the fields in the config are not set then a nil object would ne returned
 func NewCognitoClient(cfg *Config) Cognito {
 	client := &awsCognito{}
-	if cfg == nil || isEmpty(cfg.UserPoolID, cfg.ClientID, cfg.IdentityPoolID, cfg.Region) {
+	if cfg == nil || empty(cfg.UserPoolID, cfg.ClientID, cfg.Region) {
 		return nil
 	}
 	client.clientID = cfg.ClientID
@@ -58,8 +68,6 @@ type awsCognito struct {
 	identityPoolID string
 }
 
-// Authenticate authenticates a user and if the authentication is successful
-// it returns the token else returns empty string but a non nil error
 func (c *awsCognito) Authenticate(userName, password string) (string, error) {
 	ctx := context.Background()
 	csrp, err := srp.NewCognitoSRP(userName, password, c.userPoolID, c.clientID, nil)
@@ -128,4 +136,70 @@ func (c *awsCognito) Authenticate(userName, password string) (string, error) {
 		return token, nil
 	}
 	return token, fmt.Errorf("Challenge %s is not handled", resp.ChallengeName)
+}
+
+func (c *awsCognito) SignUp(userName, emailID, password string) (string, error) {
+	ctx := context.Background()
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return "", fmt.Errorf("unable to load default AWS Config: %w", err)
+	}
+	cfg.Region = c.region
+	cfg.Credentials = aws.AnonymousCredentials
+
+	if emailID == "" || c.userPoolID == "" || userName == "" {
+		return "", errors.New("email ID, username or password is empty")
+	}
+	cognitoClient := cip.New(cfg)
+
+	inp := &cip.SignUpInput{
+		ClientId: aws.String(c.clientID),
+		Username: aws.String(userName),
+		Password: aws.String(password),
+		UserAttributes: []cip.AttributeType{
+			{
+				Name:  aws.String("email"),
+				Value: aws.String(emailID),
+			},
+		},
+	}
+	reqs := cognitoClient.SignUpRequest(inp)
+	resps, err := reqs.Send(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	fmt.Println(resps.String())
+
+	return "", nil
+}
+
+func (c *awsCognito) ConfirmSignUp(userName, confirmationCode string) (string, error) {
+	ctx := context.Background()
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return "", fmt.Errorf("unable to load default AWS Config: %w", err)
+	}
+	cfg.Region = c.region
+	cfg.Credentials = aws.AnonymousCredentials
+
+	if c.userPoolID == "" || userName == "" || confirmationCode == "" {
+		return "", errors.New("username or confirmation code is empty")
+	}
+	cognitoClient := cip.New(cfg)
+
+	csui := &cip.ConfirmSignUpInput{
+		ClientId:         aws.String(c.clientID),
+		Username:         aws.String(userName),
+		ConfirmationCode: aws.String(confirmationCode),
+	}
+	req := cognitoClient.ConfirmSignUpRequest(csui)
+	res, err := req.Send(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	fmt.Println(res.String())
+
+	return "", nil
 }
